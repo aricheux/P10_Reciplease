45f9e8ec3268606e1050f56f3d4f6f362ecd9aac
Richeux Antoine <richeux.antoin44@gmail.com>
2018-04-17 08:27:35 +0200

    Initial Commit

---
+++ P10_Reciplease/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  P10_Reciplease
+//
+//  Created by RICHEUX Antoine on 17/04/2018.
+//  Copyright © 2018 Richeux Antoine. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
